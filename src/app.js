479e2eb92e8ad4b9d3d98e6aa61de8115cb2b937
Prateek Labroo <prateeklabroo123@gmail.com>
2019-11-02 02:26:19 +0530

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
+const port = process.env.PORT || 3000;
+
 
 //Paths for Express Config
 const publicFileDirectory = path.join(__dirname,'../public');
@@ -86,6 +88,6 @@ app.get('*',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
-    console.log("Server started at 3000");
+app.listen(port,()=>{
+    console.log("Server started at "+port);
 })
